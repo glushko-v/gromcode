07b214ea4ad4f14719ad7c1d85b3d3b3d992a6db
Vladimir Glushko <glushko@mail.com>
2018-02-15 16:40:48 +0200

    lesson 9 - homework

---
+++ src/lesson9/HomeWork/UserRepository.java
@@ -0,0 +1,19 @@
+package lesson9.HomeWork;
+
+public class UserRepository {
+    private User[] users;
+
+    public UserRepository(User[] users) {
+        this.users = users;
+    }
+
+    public User[] getUsers() {
+        return users;
+    }
+
+    public User[] getUsernames(){
+        return users;
+    }
+
+    
+}
