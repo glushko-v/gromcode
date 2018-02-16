d183c95841c563687acfa18f224b81f9c5e61bea
Vladimir Glushko <glushko@mail.com>
2018-02-16 22:51:42 +0200

    lesson 9 homework

--- src/lesson9/HomeWork/UserRepository.java
+++ src/lesson9/HomeWork/UserRepository.java
@@ -11,11 +11,18 @@ public class UserRepository {
         return users;
     }
 
-    public String[] getUsernames() {
+    public String[] getUserNames() {
         String[] userNames = {"John", "Jack", "Dan", "Bill", "Hannah"};
         return userNames;
 
     }
+
+    public long[] getUserIds(){
+        long[] userIds = {5648, 98751, 112, 3654, 882};
+        return userIds;
+    }
+
+    
 
 
 }
