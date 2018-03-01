b67e65e2d13b91708ac721792265fb86f25a1401
Vladimir Glushko <glushko@mail.com>
2018-03-01 19:58:45 +0200

    lesson 10

--- src/lesson10/HomeWork/Demo.java
+++ src/lesson10/HomeWork/Demo.java
@@ -6,14 +6,24 @@ public class Demo {
     public static void main(String[] args) {
         ElectronicsOrder electronicsOrder = new ElectronicsOrder("Calculator", new Date(), "Kiev", "Odessa",
                 100, new Customer("Julia", "Odessa", "Female"), 12);
+        ElectronicsOrder electronicsOrder1 = new ElectronicsOrder("Player", new Date(), "Odessa", "Lvov",
+                350, new Customer("Denis", "Lvov", "Male"), 24);
         FurnitureOrder furnitureOrder = new FurnitureOrder("Table", new Date(), "Lvov", "Nikolaev",
                 1500, new Customer("Ivan", "Nikolaev", "Male"), "441");
+        FurnitureOrder furnitureOrder1 = new FurnitureOrder("Chair", new Date(), "Kiev", "Odessa",
+                1000, new Customer("Evgenia", "Odessa", "Female"), "387");
 
         electronicsOrder.validateOrder();
         electronicsOrder.calculatePrice();
 
+        electronicsOrder1.validateOrder();
+        electronicsOrder1.calculatePrice();
+
         furnitureOrder.validateOrder();
         furnitureOrder.calculatePrice();
+
+        furnitureOrder1.validateOrder();
+        furnitureOrder1.calculatePrice();
 
     }
 }
