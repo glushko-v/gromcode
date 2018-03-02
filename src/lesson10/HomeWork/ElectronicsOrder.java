303d4af1d81f868e642c7e383ed67e9cb8cbfe4f
Vladimir Glushko <glushko@mail.com>
2018-03-02 02:00:41 +0200

    lesson 10

--- src/lesson10/HomeWork/ElectronicsOrder.java
+++ src/lesson10/HomeWork/ElectronicsOrder.java
@@ -11,27 +11,34 @@ public class ElectronicsOrder extends Order {
         this.guaranteeMonths = guaranteeMonths;
     }
 
-    @Override
-    void calculatePrice() {
-        double shipPrice = getTotalPrice() - getBasePrice();
-        if (getShipToCity() != "Kiev" || getShipToCity() != "Odessa") shipPrice = getBasePrice()*0.15;
-        else shipPrice = getBasePrice()*0.1;
-    }
 
     @Override
     void validateOrder() {
 
-        String[] cities = {"Kiev", "Odessa", "Dnepr", "Kharkov"};
-
-
-        for (String city : cities) {
-            if (city == getShipFromCity()) setDateConfirmed(new Date());
-            if (city == getShipToCity()) setDateConfirmed(new Date());
-
-        }
+//        String[] cities = {"Kiev", "Odessa", "Dnepr", "Kharkov"};
+//
+//
+//        for (String city : cities) {
+//            if (city == getShipFromCity()) setDateConfirmed(new Date());
+//            if (city == getShipToCity()) setDateConfirmed(new Date());
+//
+//        }
+//        if (getBasePrice() >= 100) setDateConfirmed(new Date());
+//        if (getCustomerOwned().getGender() == "Female") setDateConfirmed(new Date());
+        if (getShipFromCity() == "Kiev" || getShipFromCity() == "Odessa" || getShipFromCity() == "Dnepr" || getShipFromCity() == "Kharkov")
+            setDateConfirmed(new Date());
+        if (getShipToCity() == "Kiev" || getShipToCity() == "Odessa" || getShipToCity() == "Dnepr" || getShipToCity() == "Kharkov")
+            setDateConfirmed(new Date());
         if (getBasePrice() >= 100) setDateConfirmed(new Date());
+        getCustomerOwned().getName();
         if (getCustomerOwned().getGender() == "Female") setDateConfirmed(new Date());
-        
 
+    }
+
+    @Override
+    void calculatePrice() {
+
+        if (getShipToCity() != "Kiev" || getShipToCity() != "Odessa") setTotalPrice(getTotalPrice()*0.15);
+        else setTotalPrice(getBasePrice() * 0.1);
     }
 }
