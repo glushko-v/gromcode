8112e455d047dc1dc522b895be2c775b7b1ce135
Vladimir Glushko <glushko@mail.com>
2018-03-12 17:03:33 +0200

    lesson 11

--- src/lesson10/abstractfirstexample/DbProvider.java
+++ src/lesson10/abstractfirstexample/DbProvider.java
@@ -1,7 +1,15 @@
 package lesson10.abstractfirstexample;
 
 public abstract class DbProvider {
+
+    private String dbHost;
+
     abstract void connectToDb();
 
     abstract void disconnectFromDb();
+
+    void printDbHost(){
+        System.out.println("Db host is" + dbHost);
+    }
+
 }
