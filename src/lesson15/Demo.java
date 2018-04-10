8dadd22ed44f87d076533e89957e933df7f3f917
Vladimir Glushko <glushko@mail.com>
2018-04-10 16:53:53 +0300

    lesson 15

---
+++ src/lesson15/Demo.java
@@ -0,0 +1,10 @@
+package lesson15;
+
+public class Demo {
+    public static void main(String[] args) {
+        File file1 = new File(111, "home/Documents/test", "txt");
+        File file2 = new File(111, "home/Documents/image", "jpg");
+
+        
+    }
+}
