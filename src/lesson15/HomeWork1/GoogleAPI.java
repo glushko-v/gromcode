2383fc08f849b956f604ee2d14c61cbfadbd8b70
Vladimir Glushko <glushko@mail.com>
2018-04-16 12:00:32 +0300

    lesson 15 - homework

---
+++ src/lesson15/HomeWork1/GoogleAPI.java
@@ -0,0 +1,61 @@
+package lesson15.HomeWork1;
+
+public class GoogleAPI implements API {
+    private Room[] rooms;
+
+    public GoogleAPI(Room[] rooms) {
+        this.rooms = rooms;
+    }
+
+
+    @Override
+    public Room[] findRooms(int price, int persons, String city, String hotel) {
+        int index = 0;
+        for (Room room : rooms) {
+            if (room != null) {
+                if (price == room.getPrice()) {
+                    if (persons == room.getPersons()) {
+                        if (city.equals(room.getCityName())) {
+                            if (hotel.equals(room.getHotelName())) {
+                                index++;
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        }
+        Room[] roomsFound = new Room[index];
+        int count = 0;
+
+        for (Room room : rooms) {
+            if (room != null) {
+                if (price == room.getPrice()) {
+                    if (persons == room.getPersons()) {
+                        if (city.equals(room.getCityName())){
+                            if (hotel.equals(room.getHotelName())) {
+                                roomsFound[count] = room;
+                                count++;
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+
+        return roomsFound;
+    }
+
+    @Override
+    public Room[] getAll() {
+        return rooms;
+    }
+
+
+}
+
+
