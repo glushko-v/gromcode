4024fe29f1fe25d32500733e426ebb00951a87f9
Vladimir Glushko <glushko@mail.com>
2018-04-17 17:16:58 +0300

    lesson 16 - homework

--- src/lesson16/HomeWork/Solution.java
+++ src/lesson16/HomeWork/Solution.java
@@ -6,21 +6,42 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args) {
 
-        String test = "";
-        System.out.println(countWords(test));
+        String test = "bla bla bla";
+        String[] results = test.split("");
+
+        for (String result: results) {
+            System.out.print(Arrays.toString(result.getBytes()));
+        }
+
+//        byte[] numbers = new byte[results.length];
+//
+//        for (int i = 0; i <results.length; i++) {
+//            numbers[i] =
+//        }
+
 
     }
 
-    public static int countWords(String input){
+    static int countWords(String input){
 
         String[] results = input.split(" ");
+
+//        for (String result: results) {
+//            byte[] numbers = result.getBytes();
+//        }
+
+
         int count = 0;
 
         for (String result: results) {
-            if (!result.equals(null) && !result.equals("")) count++;
+            if (result != null && !result.equals("")) count++;
         }
 
         return count;
 
     }
+
+
+
+
 }
