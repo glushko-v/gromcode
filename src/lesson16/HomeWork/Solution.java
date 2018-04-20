16e352a8db425bef682deb8b1b4d96f99ec174a0
Vladimir Glushko <glushko@mail.com>
2018-04-20 15:04:07 +0300

    lesson 16 - homework

--- src/lesson16/HomeWork/Solution.java
+++ src/lesson16/HomeWork/Solution.java
@@ -2,57 +2,31 @@ package lesson16.HomeWork;
 
 import java.util.Arrays;
 
+
 public class Solution {
     public static void main(String[] args) {
 
         String test = "bla bla bla !@# d@ bla bl12";
-        String[] results = test.split(" ");
+
+        String[] words = test.split(" ");
+        char[] letters = test.toCharArray();
         int count = 0;
 
-
-        for (int i = 0; i < results.length; i++) {
-            byte[] res = results[i].getBytes();
-
-
-            for (int j = 0; j < res.length; j++) {
-                if (res[j] <= 65 && res[j] >= 90) break;
-                else if (res[j] <= 97 && res[j] >= 122) break;
-                else if ((res[j] >= 65 && res[j]<= 90) || (res[j] >= 97 && res[j] <= 122))
-                System.out.print(res[j] + " ");
-
-            }
-
-
-//            for (int j = 0; j <res.length; j++) {
-//                if (res[j] == 98) break;
-//                else System.out.print(res[j] + " ");
-//            }
-
+        for (char letter: letters) {
+            if (!Character.isWhitespace(letter) && !Character.isLetter(letter))count++;
         }
 
+        char[] symbols = new char[count];
 
-
-//        for (String result: results) {
-//            System.out.print(Arrays.toString(result.getBytes()));
-//        }
-
-
-//        byte[] numbers = new byte[results.length];
-//
-//        for (int i = 0; i <results.length; i++) {
-//            numbers[i] =
-//        }
+        
 
 
     }
 
+
     static int countWords(String input) {
 
         String[] results = input.split(" ");
-
-//        for (String result: results) {
-//            byte[] numbers = result.getBytes();
-//        }
 
 
         int count = 0;
@@ -64,13 +38,6 @@ public class Solution {
         return count;
 
     }
-
-//    static int validateWords()
-    //1. Пробежаться по всем элемента массива
-    //2. Если элемент содержит символ, то его не учитывать
-    // Каждый элемент массива преобразовываем в символьный массив?
-    // Каждый элемент массива преобразовываем в массив байтов, потом определяем находятся ли числа в нужном диапазоне
-    // если элемент массива попадает в диапазон - continue, если нет - return
 
 
 }
