589319a134300604ad613ad29675a5a545433065
Vladimir Glushko <glushko@mail.com>
2018-04-23 15:58:43 +0300

    lesson 16 - homework

--- src/lesson16/HomeWork/Solution.java
+++ src/lesson16/HomeWork/Solution.java
@@ -6,10 +6,12 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args) {
 
-        String test = "boom crash bla bla d@ bla lalalalalalalala bl12 aa sd lala вл";
+        String test = "boom crash bla bla d@ bla @#jjjj 124count count555 lalalalalalalala bl12 aa sd lala вл";
 
-        System.out.println(maxWord(test));
-        System.out.println(minWord(test));
+//        System.out.println(maxWord(test));
+//        System.out.println(minWord(test));
+//        isWord(test);
+        System.out.println(countWords(test));
 
 
     }
@@ -23,7 +25,11 @@ public class Solution {
         int count = 0;
 
         for (String result : results) {
-            if (result != null && !result.equals("")) count++;
+
+            if (result != null && !result.equals("")) {
+//                isWord(result);
+                count++;
+            }
         }
 
         return count;
@@ -62,6 +68,23 @@ public class Solution {
         }
 
         return minWord;
+
+    }
+
+    static void isWord(String input) {
+        String[] results = input.split(" ");
+        for (int i = 0; i < results.length; i++) {
+            char[] symbols = results[i].toCharArray();
+            for (char symbol : symbols) {
+                if (!Character.isLetter(symbol)) {
+                    results[i] = null;
+
+                }
+
+
+            }
+            System.out.print(results[i] + " ");
+        }
 
 
     }
