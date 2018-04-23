96ebf321f9f95dd570afb869a4ada9ee9301f7ba
Vladimir Glushko <glushko@mail.com>
2018-04-23 16:29:34 +0300

    lesson 16 - homework

--- src/lesson16/HomeWork/Solution.java
+++ src/lesson16/HomeWork/Solution.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args) {
 
-        String test = "boom crash aaaaaaaaa bla bla d@ bla @#jjjj 124count count555 la bl12 aa sd lala вл";
+        String test = "boom crash aaa bla bla d@ bla @#jjjj 124count count555 la bl12 aa sd lala вл";
 
 //        System.out.println(maxWord(test));
 //        System.out.println(minWord(test));
