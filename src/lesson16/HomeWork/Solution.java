9ebb5e8e93c3045139dea9c3e95f70bcc2a3c70c
Vladimir Glushko <glushko@mail.com>
2018-05-07 23:31:44 +0300

    lesson 16

--- src/lesson16/HomeWork/Solution.java
+++ src/lesson16/HomeWork/Solution.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args) {
 
-        String test = "https://www.mail.org";
+        String test = "https://government.org";
 
 //        System.out.println(maxWord(test));
 //        System.out.println(minWord(test));
