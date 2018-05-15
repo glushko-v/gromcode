4b5f7d9e8d747955043ddc33c49156fe7817c37c
Vladimir Glushko <glushko@mail.com>
2018-05-15 13:03:11 +0300

    lesson 17

--- src/lesson17/HomeWork/Solution.java
+++ src/lesson17/HomeWork/Solution.java
@@ -5,38 +5,60 @@ import java.util.Arrays;
 public class Solution {
 
     public static void main(String[] args) {
-        String test = "123 a121 134 452";
-        System.out.println(Arrays.toString(findNumbers(test)));
+        String test = "aaa 123 a 134 452 888";
+        findNumbers(test);
 
     }
 
 
-    static int[] findNumbers(String text) {
-
-        //разбиваем на массив стрингов
-
-
+    static int[] findNumbers(String text)  {
         String[] words = text.split(" ");
         int count = 0;
-
         for (String word: words) {
-            if(isDigit(word)) count++;
+            if(isDigit(word))count++;
         }
+
+
 
         int[] numbers = new int[count];
 
-
-        try {
-            for (int i = 0; i < words.length; i++) {
-
-                if (isDigit(words[i])) numbers[i] = Integer.parseInt(words[i]);
-
+        for (String word : words) {
+            for (int number: numbers) {
+                if (isDigit(word)){number = Integer.parseInt(word);
+                System.out.println(number);}
+                else System.out.println("not number");
             }
-        }catch (Exception e){
-            System.out.println("Err");
         }
         return numbers;
     }
+
+
+//    static int[] findNumbers(String text) {
+//
+//
+//
+//
+//        String[] words = text.split(" ");
+//        int count = 0;
+//
+//        for (String word: words) {
+//            if(isDigit(word)) count++;
+//        }
+//
+//        int[] numbers = new int[count];
+//
+//
+//        try {
+//            for (int i = 0; i < words.length; i++) {
+//
+//                if (isDigit(words[i])) numbers[i] = Integer.parseInt(words[i]);
+//
+//            }
+//        }catch (Exception e){
+//            System.out.println("Err");
+//        }
+//        return numbers;
+//    }
 
     static boolean isDigit(String input){
 
