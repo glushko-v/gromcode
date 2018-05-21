ee795d2483f038c42edb91c2bc7f58eb273aaf5b
Vladimir Glushko <glushko@mail.com>
2018-05-21 13:25:33 +0300

    lesson 19- homework

--- src/lesson19/HomeWork/Demo.java
+++ src/lesson19/HomeWork/Demo.java
@@ -6,14 +6,14 @@ public class Demo {
     public static void main(String[] args) {
 
         File file1 = new File(123456789, "test", "txt", 545);
-        File file2 = new File(222222222, "John", "jpeg", 4545);
+        File file2 = new File(222222222, "John", "jpg", 4545);
         File file3 = new File(112, "guide", "doc", 788);
         File file4 = new File(321, "ttt", "txt", 211);
 
         Controller controller = new Controller();
 
         File[] files = {file1, file2, file3};
-        File[] files1 = {null, null, null};
+        File[] files1 = {null, null, null, null};
         String[] formatsSupported = {"txt", "jpg", "doc"};
 
         Storage storage = new Storage(789, files, formatsSupported, "Ukraine", 999999999);
@@ -21,6 +21,8 @@ public class Demo {
 
         controller.delete(storage, file1);
         controller.put(storage, file4);
+
+        System.out.println(storage.toString());
 
         controller.transferAll(storage, storage1);
 
