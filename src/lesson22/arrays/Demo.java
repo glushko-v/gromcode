704d2ab8f0fd796d77fe32a4ff0dd5f57c6de0b6
Vladimir Glushko <glushko@mail.com>
2018-06-27 14:02:51 +0300

    lesson 22 - homework

---
+++ src/lesson22/arrays/Demo.java
@@ -0,0 +1,9 @@
+package lesson22.arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+        int[] array = {1, 10, 5, 6, 4, 4, 3};
+
+        System.out.println(ArrayUtils.nCount(array, 4));
+    }
+}
