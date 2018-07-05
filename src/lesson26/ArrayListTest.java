55a57a20a86430ad0467db0f99c7632544017fd4
Vladimir Glushko <glushko@mail.com>
2018-07-05 10:08:03 +0300

    lesson 26

--- src/lesson26/ArrayListTest.java
+++ src/lesson26/ArrayListTest.java
@@ -5,7 +5,14 @@ import java.util.List;
 
 public class ArrayListTest {
 
-    List useList(Order order, int index, Order order1, int index1, Order order2){
+    List useList(){
+
+        Order order = new Order(11, 250, "UAH", "Kettle", "5465df");
+        Order order1 = new Order (12, 100, "UAH", "Pan", "aaa87");
+        Order order2 = new Order(13, 150, "UAH", "Fan", "1234ggg");
+        int index = 1;
+        int index1 = 5;
+                
 
         ArrayList<Order> list = new ArrayList<>();
         ArrayList<Order> list1 = new ArrayList<>();
