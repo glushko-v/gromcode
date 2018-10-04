28f651cb10f0996d21cc8762e2a90002eed55aa9
glushko-v <glushko@mail.com>
2018-10-04 14:45:58 +0300

    lesson 34 - homework 1.1

--- src/lesson34/HomeWork/Demo.java
+++ src/lesson34/HomeWork/Demo.java
@@ -8,11 +8,9 @@ public class Demo {
 
         Solution solution = new Solution();
 
-//        solution.transferSentences("C:\\TEST\\test.txt", "C:\\TEST\\test2.txt");
-//        System.out.println(Arrays.toString(solution.validateSentence("Hello.My name is nigga mazafaka.I'm a hussla.Yep")));
-//       solution.deleteContent("C:\\TEST\\test.txt");
 
-//        solution.copyFileContent("C:\\TEST\\test.txt", "C:\\TEST\\test2.txt");
+
+
         solution.copyFileContentApacheIO("C:\\TEST\\test.txt", "C:\\TEST\\test2.txt");
 
 
