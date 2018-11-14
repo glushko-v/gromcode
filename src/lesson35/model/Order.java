a2403c8200c0d9f54c2283b0f08962439fb8efff
glushko-v <glushko@mail.com>
2018-11-14 15:18:15 +0200

    Core project: new abstract class for repositories

--- src/lesson35/model/Order.java
+++ src/lesson35/model/Order.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import static lesson35.model.Room.date1;
 import static lesson35.model.Room.date2;
+import static lesson35.model.Room.date4;
 
 public class Order {
     private long id;
@@ -53,6 +54,7 @@ public class Order {
 
     public static Order order1 = new Order(111, User.user1, Room.room1, date1, new Date(), 100);
     public static Order order2 = new Order(222, User.user2, Room.room2, date2, new Date(), 100);
+    public static Order order3 = new Order(333, User.user3, Room.room3, date4, new Date(), 100);
 
 
 }
--- src/lesson35/repository/UserRepository.java
+++ src/lesson35/repository/UserRepository.java
@@ -70,5 +70,7 @@ public class UserRepository extends Repository {
 
 
 
+
+
     }
 }
